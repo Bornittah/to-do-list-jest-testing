8a869072ee8a4844aedf6865ab8c3cfc5a6575b7
bornittah <bornittah@gmail.com>
2022-06-16 12:16:20 +0300

    configure jest

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # to-do-list-jest-testing
+ "To-do-list" is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete. The aim of building this project is to learn how to test the javascript functions using [Jest](https://jestjs.io/) libray
+ 
+ # Delopyment
+ Click [here](https://bornittah.github.io/to-do-list/) to check out the online version
+
+ # Built With
+- Languages - HTML, CSS and Javascript
+- Frameworks - Jest
+- Technologies - Web Technology
+- Other tools - webpack
+- npm as software packages system
+# Getting started
+### Prerequisites
+One should have knowledge about the following
+- HTML, CSS, Javascript, npm and webpack
+
+### Run
+- Navigate to the local folder and clone the repository from the github 
+- Run "git clone [url](https://github.com/Bornittah/to-do-list-jest-testing.git)"
+- Navigate to the cloned project
+- On your terminal Run "npm start"
+
+# Contributing
+Feel free to check out the [Issues page](https://github.com/Bornittah/to-do-list-jest-testing/issues)
+
+# Show your support
+Give a star if you like this project
+
+# Acknowledgement
+ [Microverse](https://www.microverse.org/?grsf=i6yi2m)
+ 
+# License
+This project is MIT licensed.
+
+# Author
+1. [Agasha Bornittah](https://github.com/Bornittah)
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: 'Development',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+    loaders: [
+      {exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/},
+      {loader: 'style-loader!css-loader', test: /\.css$/},
+      {loader: 'url-loader', test: /\.gif$/},
+      {loader: 'file-loader', test: /\.(ttf|eot|svg)$/},
+    ],
+  },
+  resolve: {
+    alias: {
+      config$: './configs/app-config.js',
+      react: './vendor/react-master',
+    },
+    extensions: ['', 'js', 'jsx'],
+    modules: [
+      'node_modules',
+      'bower_components',
+      'shared',
+      '/shared/vendor/modules',
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+};
